4240aff53a54ba95c26155ee0ffef9cf572e2936
KricheZZZ <larikkovgan2006@mail.ru>
2026-03-26 00:51:24 +0300

    fix

--- view.php
+++ view.php
@@ -26,7 +26,7 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Сохранённые анкеты | Лабораторная работа №4</title>
+    <title>Сохранённые анкеты </title>
     <link rel="stylesheet" href="style.css">
     <style>
         table {
@@ -68,7 +68,7 @@ try {
 </head>
 <body>
 <div class="container">
-    <h1>📊 Сохранённые анкеты</h1>
+    <h1>Сохранённые анкеты</h1>
     <p>Всего записей: <?= count($applications) ?></p>
 
     <table>
